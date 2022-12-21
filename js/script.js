6364e313a9414ba26eb71ec45994ab24064e96db
MarnieBenalia <marnie.benalia@gmail.com>
2022-12-21 16:16:27 +0100

    workday01

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let input = document.querySelector("input"),
     Button = document.querySelector("button"),
-    cityEnter = document.querySelector("h1");
+    cityEnter = document.querySelector("h1"),
+    temps = document.querySelector("p"),
+    icon01 = document.querySelector("img")
+    date = document.querySelector("h3")
 
 Button.addEventListener('click',() =>{
 
@@ -9,19 +12,15 @@ Button.addEventListener('click',() =>{
     search(city)
     .then((r => r.json ()))
     .then((json)=>{
-    cityEnter.textContent = json.name
 
+    if (city == '' ){
+
+        window.alert("attention champ vide");
+    }    
+        cityEnter.textContent = json.name,
+        temps.textContent = json.weather[0].description,
+        icon01.src = json.weather[0].icon,
+        date.textContent = json.dt
     })
 })
 
-/*description: "broken clouds"
-​​​
-icon: "04d"
-​​​
-id: 803
-​​​
-main: "Clouds"*/
-
-/*fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=93470c9d125ec60da741809a54f81589')
-    .then(r =>r.json())
-    .then(json => console.log(json))*/
