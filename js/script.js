6f824413dbd4647f938604512fa26823c0d869c2
Marnie Benalia <marnie.benalia@gmail.com>
2022-12-22 11:15:05 +0100

    workday2

--- js/script.js
+++ js/script.js
@@ -9,20 +9,30 @@ let input = document.querySelector("input"),
 Button.addEventListener('click',() =>{
 
     let city = input.value
-    let search= (city) => fetch ('http://api.openweathermap.org/data/2.5/weather?q='+city+'&APPID=93470c9d125ec60da741809a54f81589')
+    let search= (city) => fetch ('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&units=metric&APPID=93470c9d125ec60da741809a54f81589')
     search(city)
     .then((r => r.json ()))
     .then((json)=>{
-
+    console.log(json)
     if (city == '' ){
 
         window.alert("attention champ vide");
     }    
-        cityEnter.textContent = json.name,
-        temps.textContent = json.weather[0].description,
-        icon01.src = json.weather[0].icon,
-        date.textContent = json.dt
-        degre.textContent = json.main.temp_max
+        cityEnter.textContent = json.city.name,
+        temps.textContent = json.list[0].weather[0].description,
+        date.textContent = json.list[0].dt_txt.split(" ")[0],
+        icon01.src = "http://openweathermap.org/img/wn/"+json.list[0].weather[0].icon+"@2x.png",
+        degre.textContent = (json.list[0].main.temp_max.toFixed(0))
     })
 })
 
+//----------------------------POUR L'API WEATHER--------------------------------//
+    /*
+        cityEnter.textContent = json.name,
+        temps.textContent = json.weather[0].description,
+        icon01.src = "http://openweathermap.org/img/wn/"+json.weather[0].icon+"@2x.png",
+        date.textContent = json.dt,
+        degre.textContent = (json.main.temp_max.toFixed(0))
+    */
+
+//----------------------------POUR L'API WEATHER--------------------------------//
